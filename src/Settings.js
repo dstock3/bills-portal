aa2f59580ea938c9e514824bc78314c195dd6c0c
David Stockdale <dstock3@gmail.com>
2022-12-12 16:53:46 -0600

    Added a go back button and adjusted stylistic aspects of settings page.

--- src/Settings.js
+++ src/Settings.js
@@ -1,21 +1,27 @@
 import React from 'react'
+import './style/settings.css'
 
-const Settings = ({difficulty, setDifficulty}) => {
+const Settings = ({setSettings, difficulty, setDifficulty}) => {
   return (
     <div className="settings">
-        
-        <div className="difficulty-container">
-          {difficulty === 3 ? 
-            <div className="difficulty selected">Easy</div> :
-            <div className="difficulty" onClick={()=>setDifficulty(3)}>Easy</div>
-          }
-          {difficulty === 5 ? 
-            <div className="difficulty selected">Medium</div> :
-            <div className="difficulty" onClick={()=>setDifficulty(5)}>Medium</div>}
-          {difficulty === 7 ? 
-            <div className="difficulty selected">Hard</div> :
-            <div className="difficulty" onClick={()=>setDifficulty(7)}>Hard</div>}
+        <h1>Settings</h1>
+
+        <div className="settings-subcontainer">
+          <h3>Difficulty</h3>
+          <div className="difficulty-container">
+            {difficulty === 3 ? 
+              <div className="difficulty selected">Easy</div> :
+              <div className="difficulty" onClick={()=>setDifficulty(3)}>Easy</div>}
+            {difficulty === 5 ? 
+              <div className="difficulty selected">Medium</div> :
+              <div className="difficulty" onClick={()=>setDifficulty(5)}>Medium</div>}
+            {difficulty === 7 ? 
+              <div className="difficulty selected">Hard</div> :
+              <div className="difficulty" onClick={()=>setDifficulty(7)}>Hard</div>}
+          </div>
         </div>
+
+        <div className="back" onClick={()=>setSettings(false)}>Go Back</div>
     </div>
   )
 }
